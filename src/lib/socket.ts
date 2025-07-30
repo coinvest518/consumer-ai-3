570eff7224254e9363b4772036f4a0a8798dcafb
coinvest518 <coinvest518@gmail.com>
2025-07-30 09:43:37 -0400

    meta

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,5 +1,5 @@
 import { io, Socket } from "socket.io-client";
 
-const socket: Socket = io(process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3001");
+const socket: Socket = io(import.meta.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3001");
 
 export default socket;
